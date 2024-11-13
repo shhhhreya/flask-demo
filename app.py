90be68491da3251a4b0b9738e2d9e0cf1b36d4cb
shhhhreya <shreyaa1101@gmail.com>
2024-11-13 22:07:11 +0530

    hello world

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+#create an instance of class Flask
+#The first argument is the name of the application’s module or package (i.e. __name__ here)
+app = Flask(__name__)  #so that Flask knows where to look for resources such as templates and static files
+
+@app.route('/')  
+def hello():
+    return "hello world" # can render html as well inside quotes
+
+if __name__ == "__main__":
+    app.run(debug=True)
