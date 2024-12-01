d6161ed139b6b0cb3d796beac02b592af5c582ba
shhhhreya <shreyaa1101@gmail.com>
2024-12-01 16:01:02 +0530

    url building

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 #create an instance of class Flask
 #The first argument is the name of the application’s module or package (i.e. __name__ here)
 app = Flask(__name__)  #so that Flask knows where to look for resources such as templates and static files
 
-@app.route('/')  
+@app.route('/')
 def index():
-    return "Index Page!" # can render html as well inside quotes
+    return 'index'
 
-@app.route('/hello/<username>')
-def hello(username):
-    return f'Hello {escape(username)}'
+@app.route('/login')
+def login():
+    return 'login'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f'Post id: {post_id}'
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    return f'subpath: {escape(subpath)}'
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+#URL Building
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
